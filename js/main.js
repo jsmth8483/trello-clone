8bb28d291edbd1f4e81406034fd527ea4e77d90c
James Smith <jameswsmith805@gmail.com>
2025-10-23 11:17:22 -0700

    removed dead code in main.js

--- js/main.js
+++ js/main.js
@@ -7,19 +7,3 @@ document.addEventListener("DOMContentLoaded", () => {
         boardPickerPane.classList.toggle("open");
     });
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const body = document.querySelector("body");
-//     const boardPickerPane = document.querySelector(".board-picker-pane");
-//     const boardPickerBtn = document.querySelector(".board-picker-btn");
-
-//     body.addEventListener("click", (e) => {
-//         if (
-//             boardPickerPane.classList.contains("open") &&
-//             e.target !== boardPickerBtn &&
-//             e.target != boardPickerPane
-//         ) {
-//             boardPickerPane.classList.remove("open");
-//         }
-//     });
-// });
